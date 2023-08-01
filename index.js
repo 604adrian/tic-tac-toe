1f538850d869400e701ab2cd0d73e818beeb0e48
Adrian Jones <adrianjones@duck.com>
2023-08-01 16:02:57 -0700

    feat: track winner with primes
    I used prime factorisation to assign each winning state of the game board a unique numerical value. Each round of the game, it is possible to mark off which spots of the array are marked-off then filter that array to extract only the elements that are numbers. From there, it is possible to multiply the whole array together. If the resulting number marks off a unique state of the game board, so it can be used to determine if the game has been won yet or not (by comparing it to the array that stores all the numerical values for winning states of the board, and returning if a match is found with the actual state of the board as stored in the xChecker or oChecker arrays).

--- index.js
+++ index.js
@@ -1,92 +1,81 @@
+// game logic
+// 0 1 2   H A L
+// 3 4 5   H A L
+// 6 7 8   H A L
+const game = {
+  board: [2, 3, 5, 7, 11, 13, 17, 19, 23],
+  xChecker: [2, 3, 5, 7, 11, 13, 17, 19, 23],
+  oChecker: [2, 3, 5, 7, 11, 13, 17, 19, 23],
+  winner: ""
+}
+
 // players
 function createPlayer(name, marker) {
   return {
     name: name,
-    marker: marker
+    marker: marker,
   };
 }
 
 const playerOne = createPlayer("billy", "X");
-const playerTwo = createPlayer("sally", "Y");
-
-// game logic
-// 0 1 2
-// 3 4 5
-// 6 7 8
-const game = {
-  board: [false, false, false, false, false, false, false, false],
-  winForX: ["X", "X", "X"],
-  winForY: ["Y", "Y", "Y"],
-  xCurrentState: [],
-  yCurrentState: [],
-  wins: [
-    // horizontal
-    [0,1,2], [3,4,5], [6,7,8],
-    // diagonal
-    [0,4,8], [6,4,2]
-    // vertical
-    [0,3,6], [1,4,7], [2,5,8]
-  ]
-}
+const playerTwo = createPlayer("sally", "O");
 
 function displayBoard(position, player) {
   let status = "";
 
-  // changing displayBoard
-  if (game.board[position] === false) {
-    console.log("test");
+  // prime factorization for identification
+  const winningValues = [7436429, 222870, 30030, 440895, 238602, 937365, 355810, 149226]
+
+  // marking free space
+  let testType = typeof game.board[position];
+  if (testType === "number") {
+
+    // update the game board
     game.board[position] = player.marker;
     status = "success"
-    console.table(game.board);
+    
+    // update player's trackers
+    if (player.marker === "X") {
+      game.xChecker[position] = "X";
+      let idNum = game.xChecker.filter(prime => prime !== "X");
+      console.log(idNum);
+      let theId = idNum.reduce(function(product, value) { return product * value; });
+      console.log(theId);
+      winningValues.forEach((win) => {
+        if (theId === win) {
+          console.log("X wins");
+          game.winner = "X";
+        }
+      });
+
+    } else if (player.marker === "O") {
+      game.oChecker[position] = "O";
+      let idNum = game.xChecker.filter(prime => prime !== "O");
+      console.log(idNum);
+      let theId = idNum.reduce(function(product, value) { return product * value; });
+      console.log(theId);
+      winningValues.forEach((win) => {
+        if (theId === win) {
+          console.log("0 wins");
+          game.winner = "O";
+        }
+      });
+
+    } else {
+      game.winner = false;
+    }
+
+    
+
   } else {
     console.log(game.board[position]);
     status = "taken!"
     console.log("ERROR: position already taken, try again!")
   }
-  
-  checkIfWon();
-
-  // return
-  let opponent = '';
-  if (player.marker === "X") {
-    opponent = "Y";
-  } else {
-    opponent = "X";
-  };
-
-  if (status = "taken!") {
-    statment = `${game.board[position]} taken by ${opponent}!`;
-  } else {
-    statment = `You just got ${game.board[position]}`
-  }
-
-
-  return {
-    statment
-  }
+  // check
+  let winner = game.winner;
+  console.log(game.winner);
+  return {winner}
 }
 
-function checkIfWon() {
-  switch (game.board) {
-    case (game.board[0] === "X" && game.board[1] === "X" && game.board[2] === "X"):
-    case (game.board[3] === "X" && game.board[4] === "X" && game.board[5] === "X"):
-    case (game.board[6] === "X" && game.board[7] === "X" && game.board[8] === "X"):
-    case (game.board[0] === "X" && game.board[4] === "X" && game.board[2] === "X"):
-    case (game.board[6] === "X" && game.board[4] === "X" && game.board[2] === "X"):
-    case (game.board[0] === "X" && game.board[3] === "X" && game.board[6] === "X"):
-    case (game.board[1] === "X" && game.board[4] === "X" && game.board[8] === "X"):
-    case (game.board[2] === "X" && game.board[5] === "X" && game.board[8] === "X"):
-      console.log("X wins, game over");
-      break;
-    case (game.board[0] === "Y" && game.board[1] === "X" && game.board[2] === "X"):
-    case (game.board[3] === "Y" && game.board[4] === "X" && game.board[5] === "X"):
-    case (game.board[6] === "Y" && game.board[7] === "X" && game.board[8] === "X"):
-    case (game.board[0] === "Y" && game.board[4] === "X" && game.board[2] === "X"):
-    case (game.board[6] === "Y" && game.board[4] === "X" && game.board[2] === "X"):
-    case (game.board[0] === "Y" && game.board[3] === "X" && game.board[6] === "X"):
-    case (game.board[1] === "Y" && game.board[4] === "X" && game.board[8] === "X"):
-    case (game.board[2] === "Y" && game.board[5] === "X" && game.board[8] === "X"):
-      console.log("Y wins, game over");
-      break;
-  }
-}
+
